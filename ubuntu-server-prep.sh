7ae7ca7f099a4b69d8f335bd2390b5871c2e1449
elijahneo <31119358+elijahneo@users.noreply.github.com>
2023-06-19 09:58:06 +0800

    Update ubuntu-server-prep.sh

--- ubuntu-server-prep.sh
+++ ubuntu-server-prep.sh
@@ -9,11 +9,11 @@ sudo chmod +x /usr/bin/yq
 sudo curl -L -o - "https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz" | sudo tar -C /usr/local/bin -xvzf - govc
 
 # Prepare nginx iso directory
-sudo chmod 766 -R /usr/share/nginx/html
+sudo chmod 777 -R /usr/share/nginx/html
 
 # Create new directory /usr/share/nginx/html/iso
 sudo mkdir /usr/share/nginx/html/iso
-sudo chmod 766 /usr/share/nginx/html/iso
+sudo chmod 777 /usr/share/nginx/html/iso
 
 sudo sed -i '/# pass PHP scripts to FastCGI server/i\        location /iso {\n                alias /usr/share/nginx/html/iso/; # directory to list\n                autoindex on;\n        }\n' /etc/nginx/sites-available/default
 
