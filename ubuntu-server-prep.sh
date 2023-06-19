fdbea363c8f5425aa8cb08abadd187a3b6a48b56
elijahneo <31119358+elijahneo@users.noreply.github.com>
2023-06-19 10:21:12 +0800

    Update ubuntu-server-prep.sh

--- ubuntu-server-prep.sh
+++ ubuntu-server-prep.sh
@@ -13,7 +13,7 @@ sudo chmod 777 -R /usr/share/nginx/html
 
 # Create new directory /usr/share/nginx/html/iso
 sudo mkdir /usr/share/nginx/html/iso
-sudo chmod 766 /usr/share/nginx/html/iso
+sudo chmod 777 /usr/share/nginx/html/iso
 
 sudo sed -i '/# pass PHP scripts to FastCGI server/i\        location /iso {\n                alias /usr/share/nginx/html/iso/; # directory to list\n                autoindex on;\n        }\n' /etc/nginx/sites-available/default
 
